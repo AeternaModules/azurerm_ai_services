cf53b1a43eb368d0c93b4f09bd3811f2da8f09a4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,76 +1,71 @@
-output "ai_serviceses" {
-  description = "All ai_services resources"
-  value       = azurerm_ai_services.ai_serviceses
-  sensitive   = true
-}
 output "ai_serviceses_custom_subdomain_name" {
-  description = "List of custom_subdomain_name values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.custom_subdomain_name]
+  description = "Map of custom_subdomain_name values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.custom_subdomain_name }
 }
 output "ai_serviceses_customer_managed_key" {
-  description = "List of customer_managed_key values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.customer_managed_key]
+  description = "Map of customer_managed_key values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.customer_managed_key }
 }
 output "ai_serviceses_endpoint" {
-  description = "List of endpoint values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.endpoint]
+  description = "Map of endpoint values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.endpoint }
 }
 output "ai_serviceses_fqdns" {
-  description = "List of fqdns values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.fqdns]
+  description = "Map of fqdns values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.fqdns }
 }
 output "ai_serviceses_identity" {
-  description = "List of identity values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.identity]
+  description = "Map of identity values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.identity }
 }
 output "ai_serviceses_local_authentication_enabled" {
-  description = "List of local_authentication_enabled values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.local_authentication_enabled]
+  description = "Map of local_authentication_enabled values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.local_authentication_enabled }
 }
 output "ai_serviceses_location" {
-  description = "List of location values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.location]
+  description = "Map of location values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.location }
 }
 output "ai_serviceses_name" {
-  description = "List of name values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.name]
+  description = "Map of name values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.name }
 }
 output "ai_serviceses_network_acls" {
-  description = "List of network_acls values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.network_acls]
+  description = "Map of network_acls values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.network_acls }
 }
 output "ai_serviceses_outbound_network_access_restricted" {
-  description = "List of outbound_network_access_restricted values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.outbound_network_access_restricted]
+  description = "Map of outbound_network_access_restricted values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.outbound_network_access_restricted }
 }
 output "ai_serviceses_primary_access_key" {
-  description = "List of primary_access_key values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.primary_access_key]
+  description = "Map of primary_access_key values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.primary_access_key }
   sensitive   = true
 }
 output "ai_serviceses_public_network_access" {
-  description = "List of public_network_access values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.public_network_access]
+  description = "Map of public_network_access values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.public_network_access }
 }
 output "ai_serviceses_resource_group_name" {
-  description = "List of resource_group_name values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.resource_group_name]
+  description = "Map of resource_group_name values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.resource_group_name }
 }
 output "ai_serviceses_secondary_access_key" {
-  description = "List of secondary_access_key values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.secondary_access_key]
+  description = "Map of secondary_access_key values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.secondary_access_key }
   sensitive   = true
 }
 output "ai_serviceses_sku_name" {
-  description = "List of sku_name values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.sku_name]
+  description = "Map of sku_name values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.sku_name }
 }
 output "ai_serviceses_storage" {
-  description = "List of storage values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.storage]
+  description = "Map of storage values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.storage }
 }
 output "ai_serviceses_tags" {
-  description = "List of tags values across all ai_serviceses"
-  value       = [for k, v in azurerm_ai_services.ai_serviceses : v.tags]
+  description = "Map of tags values across all ai_serviceses, keyed the same as var.ai_serviceses"
+  value       = { for k, v in azurerm_ai_services.ai_serviceses : k => v.tags }
 }
 
